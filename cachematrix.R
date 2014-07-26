8b08740b9335bc3441f73be5a3ded05dd395e5c9
oceanamor <oceanamor@users.noreply.github.com>
2014-07-26 13:58:23 -0400

    Programming Assignment 2 validated and updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## THIS PROGRAM WILL TAKE A MATRIX AND CALCULATE IT'S INVERSE 
+## IT WILL THEN STORE THE INVERSE MATRIX IN THE CACHE AND 
+## UTILIZE THAT VALUE RATHER THAN RECALCULATING THE MATRIX
 
-## Write a short comment describing this function
+#think of this function as list of commands that creates functions to
+#1.build the matrix
+#2.creates a function to call the matrix
+#3.creates a function to calculate the inverse of the matrix
+#and then store the result of the matrix inside the function as a "private"
+#variable to the function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        invs <- NULL
+        #1 set the the matrix
+        set <- function(y) {
+                x <<- y
+                invs <<- NULL
+        }
+        #2 gets the matrix
+        get <- function() x
+        #3 sets the inverse
+        setsolve <- function(solve) invs <<- solve
+        #4 gets the inverse
+        getsolve <- function() invs
+        #creates the list output
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
+## This function determines if the inverse of the matrix has already been calculated and if it hasn't 
+## then it caculates the inverse of the matrix if the procedure has alread been completed then it 
+##used the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invs <- x$getsolve()
+        #does a value for the inversion of the matrix already exist? if Y return cached data
+        if(!is.null(invs)) {
+                message("matrix inverted already getting cached data")
+                return(invs)
+        }
+        #if solve has not already been performed then invert the matrix
+        data <- x$get()
+        invs <- solve(data, ...)
+        x$setsolve(invs)
+        invs
 }
+
+##testing github version changes by adding this comment
